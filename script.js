266986e09894d5e3b3573e5ce095af24d7c390a0
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2023-08-02 20:25:55 -0300

    create html sctructure

---
+++ script.js
@@ -0,0 +1,10 @@
+const weapon = ["Rock", "Paper", "Scissors"];
+
+getComputerChoice = () => {
+  const computerRandom = weapon[Math.floor(Math.random() * weapon.length)];
+  return computerRandom;
+};
+
+playRounds = (playerSelection, computerSelection) => {
+  switch 
+}
